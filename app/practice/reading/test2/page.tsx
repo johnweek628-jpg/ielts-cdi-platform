38246b68913206d20190db81b13651a75d057f09
John Week <johnweek628@gmail.com>
2026-03-13 14:20:02 +0500

    fix test2 module error

---
+++ app/practice/reading/test2/page.tsx
@@ -0,0 +1,7 @@
+export default function Test2Page() {
+  return (
+    <div>
+      <h1>Reading Test 2</h1>
+    </div>
+  )
+}
---
+++ app/practice/reading/test3/page.tsx
@@ -0,0 +1,7 @@
+export default function Test3Page() {
+  return (
+    <div>
+      <h1>Reading Test 3</h1>
+    </div>
+  )
+}
