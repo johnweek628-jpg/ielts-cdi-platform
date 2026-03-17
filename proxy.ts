547a581020bf26fb584b66bd45db4d7aaabc0c44
John Week <johnweek628@gmail.com>
2026-03-17 11:26:12 +0500

    ready

--- proxy.ts
+++ proxy.ts
@@ -1,21 +1,21 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
+// ✅ MUHIM: named export "proxy"
+export function proxy(request: NextRequest) {
+
+  console.log("🔥 PROXY WORKING:", request.nextUrl.pathname)
 
   const url = request.nextUrl.clone()
 
-  // faqat reading testlar uchun ishlaydi
   if (url.pathname.startsWith('/practice/reading/test')) {
 
-    // TEST NUMBER olish
     const match = url.pathname.match(/test(\d+)/)
     const testNumber = match ? Number(match[1]) : 1
 
-    // COOKIE dan plan olish (keyin backenddan ham qilamiz)
     const plan = request.cookies.get('plan')?.value || 'free'
 
-    const limits: any = {
+    const limits: Record<string, number> = {
       free: 2,
       basic: 10,
       premium: 25,
