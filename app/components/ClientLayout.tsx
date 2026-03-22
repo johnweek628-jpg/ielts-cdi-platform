1f7e760001c882bf9e629c23a5e94b717543688a
John Week <johnweek628@gmail.com>
2026-03-22 09:58:57 +0500

    d

--- app/components/ClientLayout.tsx
+++ app/components/ClientLayout.tsx
@@ -4,10 +4,13 @@ import { useState } from "react"
 import Navbar from "./Navbar"
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  const [sidebarOpen, setSidebarOpen] = useState(true)
+ 
 
   return (
     <div className="min-h-screen w-full">
+
+      {/* NAVBAR */}
+      <Navbar />
 
       {/* LAYOUT */}
       <div className="flex">
@@ -16,7 +19,6 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
         <main
           className={`
             transition-all duration-300 ease-in-out w-full pt-16
-            ${sidebarOpen ? "ml-64" : "ml-0"}
           `}
         >
           {children}
